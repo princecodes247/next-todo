0147527074aa0659b16769471a61b3bc26d1bc9e
Prince Codes <princecodes247@gmail.com>
2022-04-07 16:59:53 +0100

    feat: create footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+import { HeartIcon } from '@heroicons/react/outline'
+
+  export default function Footer() {
+    return (
+      <footer className="p-6 flex items-center justify-center text-center text-gray-700 font-semibold">
+        <HeartIcon className="w-6"/>
+        <p>
+        Built with love by PrinceCodes247
+        </p>
+        </footer>
+    )
+  }
+  
